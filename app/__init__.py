2a17b5864a5e5f1c92f1878df555589a5ba90ef5
Betico03 <104890165+Betico03@users.noreply.github.com>
2025-11-30 14:33:44 +0100

    CBP - configuracion de arranque de la APP

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,17 @@
 #archivo que indica que app es el paquete de la APP
+from flask import Flask #importamos la clase Flask
+
+def create_app(): #funcion para crear la APP
+    app=Flask(__name__) #instancio APP como objeto Flask
+
+    #configuracion del proyecto
+    app.config.from_mapping( #mapeo
+        DEBUG=True, #debug activado
+        SECRET_KEY='dev'#key para DB
+    )
+
+    @app.route('/') #vista inicio
+    def inicio(): #funcion inicio
+        return "¡Hola, Mundo!"
+    
+    return app #salida de la funcion
--- run.py
+++ run.py
@@ -1,1 +1,6 @@
 #archivo para ejecutar la APP
+from app import create_app #importamos la funcion create_app desde el paquete app
+
+if __name__=='__main__': #si el archivo es main
+    app=create_app() #al objeto APP le pasamos la funcion create_app
+    app.run() #ejecutamos APP
